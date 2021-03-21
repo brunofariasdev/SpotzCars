0da6da05f71579376434d837fc15ddc659bfb0d0
brunofariasdev <contato@brunofarias.net>
2021-03-21 20:23:29 -0300

    menu mobile

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <span>SpotzCars</span>
-      <div class="menu">
-        <router-link class="menu-item" to="/">Home</router-link> 
-        <router-link class="menu-item" to="/about">Buscar</router-link>
-        <router-link class="menu-item" to="/about">Sobre</router-link>
+    <div class="nav">
+      <div class="menu-mb">
+        <span>spotzCar</span>
+        <i @click="activemb()" :class="mbicon"></i>
       </div>
     </div>
-    <router-view id="view" />
+    <div class="menuactive" :style="activemenu">
+      <ul>
+        <router-link class="itemmb" to="/">Inicio</router-link>
+        <router-link class="itemmb" to="/about">Sobre</router-link>
+        <router-link class="itemmb" to="/search">Buscar</router-link>
+      </ul>
+    </div>
+    <router-view id="view"/>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      activemenu: '',
+      mbicon:'fas fa-bars'
+    }
+  }, 
+  methods:{
+    activemb(){
+      if(this.activemenu == ''){
+        this.activemenu = 'display:block'
+        this.mbicon = 'fas fa-times'
+      }else{
+        this.activemenu = ''
+        this.mbicon = 'fas fa-bars'
+      }
+    }
+  }
+}
+</script>
 
 <style>
 *{
@@ -27,26 +54,70 @@
   flex-direction: column;
   height: 100vh;
   width: 100vw;
-  overflow: hidden !important;
+  overflow-x: hidden !important;
 }
-#view{
-  overflow-x: auto;
+/*------------NAV-MB--------------*/
+.nav{
+  position: fixed;
+  background: transparent;
+  z-index: 3;
 }
-#nav{
-  overflow: hidden;
+.menu-mb{
+  display: flex;
   background: transparent;
   position: fixed;
-  top: 0;
-  width: 100%;
+  height: 3rem;
+  align-items: center;
+  justify-content: space-around;
+  width: 100vw;
+  z-index: 3;
+}
+.menu-mb span{
+  color: white;
+  font-weight: bold;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  font-size: 1.3rem;
+  z-index: 3;
+}
+.menu-mb i{
+  font-size: 1.8rem;
+  color:white;  
+  z-index: 3;
+}
+.menuactive{
+  margin-top: 3rem;
+  display: none;
+  height: 100vh;
+  width: 100vw;
+  position: absolute;
+  animation-name: animemenu;
+  animation-duration: .8s;
+  background-color: rgb(255, 255, 255);
+  border-radius: 1rem 1rem 0 0;
+  z-index: 3;
+  position: fixed;
+}
+@keyframes animemenu{
+  to{margin-top: 3rem;}
+  from{margin-top: 80rem;}
+}
+.menuactive ul{
+  margin-top: 2rem;
   display: flex;
-  flex-direction: row;
-  height: 4rem;
-  background-color:transparent;
+  flex-direction: column;
+  z-index: 3;
 }
-.menu-item{
-  color: red;
+.itemmb{
+  color: #47494bab;
   text-decoration: none;
-  padding: .3rem;
+  list-style: none;
+  text-align: start;
+  z-index: 3;
   font-size: 1.2rem;
+  font-weight: 300;
+  padding: .5rem;
+  
 }
+
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <div class="home-carousel">
-
+    <div class="home-carousel" :style="`background-image:url(${imgscarousel[`${img}`]})`">
     </div>
   </div>
 </template>
@@ -11,19 +10,45 @@
 
 export default {
   name: "Home",
+  data(){
+    return {
+      imgscarousel: [
+        'https://images.unsplash.com/photo-1493238792000-8113da705763?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHw%3D&w=1000&q=80',
+        'https://revistacarro.com.br/wp-content/uploads/2019/09/Ferrari-F8-Spider_3-1024x614.jpg','https://carrosecarros.com.br/upload/noticias/imagens/bYNMbV6kdb.jpg','https://cdn.motor1.com/images/mgl/rv6v6/s1/volkswagen-golf-8-gti-clubsport-2021.jpg'
+      ],
+      img: 0
+    }
+  },
   components: {
     
   },
+  computed:{
+    
+    
+  }
 };
 </script>
 
 <style scoped>
+/*------------HOME-MAIN-------------*/
 .home{
   height: 100vh;
+  z-index: 2;
 }
 .home-carousel{
   height: 100vh;
+  z-index: 2;
   background-size: cover;
-  background-image: url('https://3.bp.blogspot.com/-MtkFHTYS8uE/VJembNWD8jI/AAAAAAAB96I/Qn95UjLHCVs/s1600/VW-Golf-2010-Prata%2B(4).jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+}
+.home-carousel:after {
+    content: '\A';
+    z-index: 2;
+    position: absolute;
+    width: 100%; height:100%;
+    top:0; left:0;
+    background:rgba(0, 0, 0, 0.349);
+    opacity: 1;
 }
 </style>
